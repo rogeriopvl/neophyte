c7fc40034d9201b499bd4150d4fe0d5ac0bb2159
Rogerio Vicente <rogerpvl@gmail.com>
2010-02-27 01:00:58 +0000

    Changed command line parsing to cmdparse gem. Created command create.

--- lib/neophyte.rb
+++ lib/neophyte.rb
@@ -2,6 +2,5 @@
 
 module Neophyte
   VERSION = '0.1'
-  
   autoload :Runner, 'neophyte/runner'
 end
--- lib/neophyte/runner.rb
+++ lib/neophyte/runner.rb
@@ -1,53 +1,62 @@
-require 'optparse'
+require 'cmdparse'
+require 'fileutils'
 
 module Neophyte
   
   class Runner
     
-    VALID_COMMANDS = %w(help)
-    VALID_OPTIONS = %w(--version --web --rubygem)
+    # for now...
+    VALID_TYPES = %w(web)
+    VALID_LANGUAGES = %w(html php)
     
-    attr_accessor :command
-    attr_accessor :options
-    attr_accessor :arguments
-    
-    def initialize(argv)
-      @argv = argv
-      parse_command
-    end
-    
-    # parses the input command
-    def parse_command
-      if @argv.length == 1
-        if @argv[0] == 'help'
-          help
-        end
-      else
-        usage
-      end
+    def initialize
+      @cmd = CmdParse::CommandParser.new(true, true)
+      @cmd.program_name = 'neo '
+      @cmd.program_version = VERSION
+      
+      generate_commands
     end
     
     def start
-      # start needed stuff
-    end
-    
-    def help
-      puts "Help info"
-    end
-    
-    def usage
-      puts "Usage: neo <command>"
-      puts "Example: neo create myproject"
-      puts "Help: neo --help"
+      @cmd.parse
     end
     
     protected
-  
-    def parsed_options?
-      opts = OptionParser.new
-      opts.on('-v', '--version') { version; exit 0 }
-      opts.on('-h', '--help') { help; exit 0 }
-      opts.on('-V', '--verbose') { @options.verbose = true }
+    
+    def generate_commands
+      
+      create = CmdParse::Command.new('create', false)
+      create.short_desc = 'Creates the folders and files for a given project name.'
+      create.description = 'This command creates all the folders and files '
+      create.description << 'for the given project name. You can specify the '
+      create.description << 'project type and main language with the -t '
+      create.description << 'and -l switches. By default -t is web and -l is html.'
+      
+      create.options = CmdParse::OptionParserWrapper.new do |opt|
+        opt.on('-t', '--type TYPE',
+          'Create project files of type TYPE') { |type| @type = type }
+        opt.on('-l', '--language LANGUAGE',
+          'Create main project files with the LANGUAGE extension') { |language| @language = language }
+      end
+      
+      create.set_execution_block do |args|
+        
+        # should this be the desired behavior? Maybe give exit if not valid
+        @language = VALID_LANGUAGES[0] unless VALID_LANGUAGES.include?(@language)
+        @type = VALID_TYPES[0] unless VALID_TYPES.include?(@type)
+        
+        puts "Type: #{@type}"
+        puts "Main Language: #{@language}"
+        self.create
+      end
+      
+      @cmd.add_command(create)
+      @cmd.add_command(CmdParse::HelpCommand.new, true)
+      @cmd.add_command(CmdParse::VersionCommand.new)
+    end
+    
+    def create
+      # TODO
     end
   
   end
